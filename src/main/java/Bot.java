babdf51b1305b025b6a30a844b31703b9931f538
SabaFathi <39127848+SabaFathi@users.noreply.github.com>
2019-04-01 14:37:01 +0430

    update Bot.java onUpdateReceived method

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,27 +1,42 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.File;
 import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
 
     private final static String TOKEN = "";
 
+    private final ConversionAdapter convertAssistant = new ConversionAdapter();
 
     public void onUpdateReceived(Update update) {
-        System.out.println(update.getMessage());
-        SendMessage msg = new SendMessage(update.getMessage().getChatId(), "welcome!");
+        if(update.hasMessage()){
+            if(update.getMessage().hasText()){
+                String message =  update.getMessage().getText().toLowerCase();
+                if(message.startsWith("/get")){
+                    File pdf = convertAssistant.getPdf();
+                    try {
+                        SendDocument sendDocument = new SendDocument()
+                                .setChatId(update.getMessage().getChatId())
+                                .setDocument(pdf);
 
-        try {
-            System.out.println("UserName: " + getMe().getUserName());
-            System.out.println("FistName: " + getMe().getFirstName());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+                        execute(sendDocument);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }else if(message.startsWith("/reset")){
+                    convertAssistant.reset();
+                }
+            }else if(update.getMessage().hasPhoto()){
+                List<PhotoSize> photo = update.getMessage().getPhoto();
+                convertAssistant.add(photo.get(0).getFilePath());
+            }
         }
-
     }
 
     public void onUpdatesReceived(List<Update> updates) {
